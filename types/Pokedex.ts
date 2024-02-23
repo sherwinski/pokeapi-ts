09473fbb4d3468dd59536f0481aafa22287827de
sherwinski <sherwinhb@gmail.com>
2024-02-23 09:29:47 +0700

    feat(types): add type for Resource and request options

--- types/Pokedex.ts
+++ types/Pokedex.ts
@@ -7,3 +7,19 @@ export type TPokedex =
       apiVersion: never;
       baseUrl: string;
     };
+
+export type TResource = {
+  apiVersion: string;
+  baseUrl: string;
+  endpoint: string;
+};
+
+export type TSearchOptions =
+  | {
+      limit?: number;
+      offset?: number;
+    }
+  | {
+      limit: never;
+      offset: string;
+    };
