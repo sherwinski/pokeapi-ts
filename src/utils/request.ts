72fbe9a231ed5a7a10b006f482f2265092e3cca9
sherwinski <sherwinhb@gmail.com>
2024-02-23 21:23:15 +0700

    refactor: make the URL construction process more DRY
    Additionally, add pagination query parameters to URL

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,17 +1,27 @@
 import { PokemonApiError } from "../error";
 import { PokemonApiErrorCodes } from "../../types/Error";
+import { TRequestParams } from "../../types/Pokedex";
 
-export default async function request(
-  url: URL,
-  options?: { endpoint: string }
-) {
+export default async function request({
+  url,
+  pathname,
+  endpoint,
+  ...options
+}: TRequestParams): Promise<any> {
+  // TODO make a more generic return type
   try {
+    if (options?.limit) url.searchParams.set("limit", String(options?.limit));
+    if (options?.offset)
+      url.searchParams.set("offset", String(options?.offset));
+
+    url.pathname += pathname;
+
     const response = await fetch(url);
     if (!response.ok) {
       if (response.status === 404) {
         throw new PokemonApiError({
           code: PokemonApiErrorCodes.RESOURCE_NOT_FOUND,
-          details: `${options?.endpoint} not found.`,
+          details: `${endpoint} not found.`,
         });
       } else {
         throw new PokemonApiError({
