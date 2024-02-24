cbf9aeecc416e3ea76fd6f5678c365371bc93167
sherwinski <sherwinhb@gmail.com>
2024-02-24 16:26:14 +0700

    test: add integration tests for error cases

--- test/integration/generation.test.ts
+++ test/integration/generation.test.ts
@@ -9,30 +9,40 @@ describe("Pokemon class", () => {
   beforeAll(() => {
     generation = new Pokedex().generation;
   });
+  describe("endpoint methods", () => {
+    it("returns data in the expected shape when invoking search()", async () => {
+      const data: GenerationResponse = await generation.search();
 
-  it("returns data in the expected shape when invoking search()", async () => {
-    const data: GenerationResponse = await generation.search();
+      expect(data).toHaveProperty("count");
+      expect(data).toHaveProperty("next");
+      expect(data).toHaveProperty("previous");
+      expect(data).toHaveProperty("results");
+    });
 
-    expect(data).toHaveProperty("count");
-    expect(data).toHaveProperty("next");
-    expect(data).toHaveProperty("previous");
-    expect(data).toHaveProperty("results");
+    it("returns data in the expected shape when invoking searchById()", async () => {
+      const data: GenerationResponse = await generation.searchById(1);
+
+      for (const field in GenerationSampleResponse) {
+        expect(data).toHaveProperty(field);
+      }
+    });
+    it("returns data in the expected shape when invoking searchByName()", async () => {
+      const data: GenerationResponse = await generation.searchByName(
+        "generation-i"
+      );
+
+      for (const field in GenerationSampleResponse) {
+        expect(data).toHaveProperty(field);
+      }
+    });
   });
 
-  it("returns data in the expected shape when invoking searchById()", async () => {
-    const data: GenerationResponse = await generation.searchById(1);
-
-    for (const field in GenerationSampleResponse) {
-      expect(data).toHaveProperty(field);
-    }
-  });
-  it("returns data in the expected shape when invoking searchByName()", async () => {
-    const data: GenerationResponse = await generation.searchByName(
-      "generation-i"
-    );
-
-    for (const field in GenerationSampleResponse) {
-      expect(data).toHaveProperty(field);
-    }
+  describe("error handling", () => {
+    it("throws an error when invoking searchById() with an invalid id", async () => {
+      await expect(generation.searchById(-1)).rejects.toThrow();
+    });
+    it("throws an error when invoking searchByName() with an invalid name", async () => {
+      await expect(generation.searchByName("generation-1")).rejects.toThrow();
+    });
   });
 });
--- test/integration/pokemon.test.ts
+++ test/integration/pokemon.test.ts
@@ -3,8 +3,6 @@ import PokemonResponse, {
   PokemonSampleResponse,
 } from "../../src/utils/pokemon.types";
 
-// Mock the global fetch
-
 describe("Pokemon Class", () => {
   let pokemon: InstanceType<TPokemonClass>;
 
@@ -12,27 +10,38 @@ describe("Pokemon Class", () => {
     pokemon = new Pokedex().pokemon;
   });
 
-  it("returns data in the expected shape when invoking search()", async () => {
-    const data: PokemonResponse = await pokemon.search();
+  describe("endpoint methods", () => {
+    it("returns data in the expected shape when invoking search()", async () => {
+      const data: PokemonResponse = await pokemon.search();
 
-    expect(data).toHaveProperty("count");
-    expect(data).toHaveProperty("next");
-    expect(data).toHaveProperty("previous");
-    expect(data).toHaveProperty("results");
+      expect(data).toHaveProperty("count");
+      expect(data).toHaveProperty("next");
+      expect(data).toHaveProperty("previous");
+      expect(data).toHaveProperty("results");
+    });
+
+    it("returns data in the expected shape when invoking searchById()", async () => {
+      const data: PokemonResponse = await pokemon.searchById(1);
+
+      for (const field in PokemonSampleResponse) {
+        expect(data).toHaveProperty(field);
+      }
+    });
+    it("returns data in the expected shape when invoking searchByName()", async () => {
+      const data: PokemonResponse = await pokemon.searchByName("pikachu");
+
+      for (const field in PokemonSampleResponse) {
+        expect(data).toHaveProperty(field);
+      }
+    });
   });
 
-  it("returns data in the expected shape when invoking searchById()", async () => {
-    const data: PokemonResponse = await pokemon.searchById(1);
-
-    for (const field in PokemonSampleResponse) {
-      expect(data).toHaveProperty(field);
-    }
-  });
-  it("returns data in the expected shape when invoking searchByName()", async () => {
-    const data: PokemonResponse = await pokemon.searchByName("pikachu");
-
-    for (const field in PokemonSampleResponse) {
-      expect(data).toHaveProperty(field);
-    }
+  describe("error handling", () => {
+    it("throws an error when invoking searchById() with an invalid id", async () => {
+      await expect(pokemon.searchById(-1)).rejects.toThrow();
+    });
+    it("throws an error when invoking searchByName() with an invalid name", async () => {
+      await expect(pokemon.searchByName("Agumon")).rejects.toThrow();
+    });
   });
 });
