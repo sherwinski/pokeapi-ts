6dd392c920e3d6e8175b93743453688b6af10fa4
sherwinski <sherwinhb@gmail.com>
2024-02-26 21:23:08 +0700

    build: configure TypeScript to generate type declaration files

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -4,5 +4,9 @@ export class PokemonApiError extends Error {
   constructor({ code, details }: PokemonApiErrorDetails) {
     super(`${code}: ${details}`);
     this.name = "PokemonApiError";
+
+    // This line is necessary for the instance to be correctly recognized as PokemonApiError type
+    // in environments that support custom error types (like Node.js and modern browsers)
+    Object.setPrototypeOf(this, PokemonApiError.prototype);
   }
 }
